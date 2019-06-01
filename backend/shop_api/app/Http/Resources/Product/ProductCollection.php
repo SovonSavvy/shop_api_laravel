1ac3c2138792ee72ba8d3c140c92aa6ebe2eea6e
Sovon Bhattacharya <sovonbhattacharya2@gmail.comm>
2019-06-01 17:51:15 +0530

    product resource and  collection done

--- backend/shop_api/app/Http/Resources/Product/ProductCollection.php
+++ backend/shop_api/app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,15 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => (1 - ( $this->discount / 100) ) * $this->price,
+            'rating' => $this->review->count() > 0 ? $this->review->sum('star')/$this->review->count() : 'No Rating yet',
+            'discount' => $this->discount,
+            'links' => [
+                'single_product' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
